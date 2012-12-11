f19623155a2043207026219463b8161f811cc0ae
Hendrik Bergunde <hbergunde@gmx.de>
2012-12-11 11:40:17 +0100

    implement sweepline algorithm

--- main.rb
+++ main.rb
@@ -1,10 +1,13 @@
 require_relative './sweep_line_status.rb'
 require_relative './rectangle.rb'
 require_relative './point.rb'
+require_relative './event_point.rb'
+
 
 require 'pry'
 
 sweep_line_status = SweepLineStatus.new
+event_point_schedule = Array.new
 
 rectangles = [
   Rectangle.new('R1', Point.new(5,10), Point.new(35,30)),
@@ -15,14 +18,26 @@ rectangles = [
   Rectangle.new('R6', Point.new(50,65), Point.new(65,90)),
   Rectangle.new('R7', Point.new(80,80), Point.new(115,90)),
   Rectangle.new('R8', Point.new(55,10), Point.new(70,30))
-].sort! { |left,right| left.top_left.x <=> right.top_left.x  }
+]
 
-event_point_schedule = Array.new rectangles
 
+rectangles.each do |rectangle|
+  event_point_schedule.push EventPoint.new(rectangle.top_left.x, rectangle)
+  event_point_schedule.push EventPoint.new(rectangle.bottom_right.x, rectangle)
+end
+
+event_point_schedule.sort! { |a,b| a.x <=> b.x }
+
+require 'pry'; binding.pry
 while not event_point_schedule.empty? do
-  r = event_point_schedule.delete_at(0)
-  puts r
+  event_point = event_point_schedule.delete_at(0)
+  break unless event_point
 
-  break unless r
+  if event_point.starts? then
+    sweep_line_status.report_intersection event_point
+    sweep_line_status.push event_point
+  else
+    sweep_line_status.delete event_point
+  end
 end
 puts 'finished.'
