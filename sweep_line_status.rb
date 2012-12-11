aaebbf9ac308b73e44cc4d35fe1fada0e013237f
Hendrik Bergunde <hbergunde@gmx.de>
2012-12-11 12:46:29 +0100

    refactor condition in sweep_line_status

--- sweep_line_status.rb
+++ sweep_line_status.rb
@@ -2,8 +2,10 @@ class SweepLineStatus < Array
   def report_intersection event_point
     self.each do |point_in_sls|
       sweepline_range = point_in_sls.rectangle.range
-      puts "Sweepline of #{point_in_sls.rectangle.name} intersects with sweepline of #{event_point.rectangle.name}" if sweepline_range.include? event_point.x
-      puts "#{point_in_sls.rectangle.name} really intersects with #{event_point.rectangle.name}" if point_in_sls.rectangle.intersect? event_point.rectangle
+      if sweepline_range.include? event_point.x
+        puts "Sweepline of #{point_in_sls.rectangle.name} intersects with sweepline of #{event_point.rectangle.name}"
+        puts "\t --> #{point_in_sls.rectangle.name} really intersects with #{event_point.rectangle.name}" if point_in_sls.rectangle.intersect? event_point.rectangle
+      end
     end
   end
 end
