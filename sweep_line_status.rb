27c3f8531669f06e31e50a3ecf2beb4bfca7505e
Hendrik Bergunde <hbergunde@gmx.de>
2012-12-11 12:39:48 +0100

    add notice if rectangles really intersect, not just their sweepline intervals

--- sweep_line_status.rb
+++ sweep_line_status.rb
@@ -3,6 +3,7 @@ class SweepLineStatus < Array
     self.each do |point_in_sls|
       sweepline_range = point_in_sls.rectangle.range
       puts "Sweepline of #{point_in_sls.rectangle.name} intersects with sweepline of #{event_point.rectangle.name}" if sweepline_range.include? event_point.x
+      puts "#{point_in_sls.rectangle.name} really intersects with #{event_point.rectangle.name}" if point_in_sls.rectangle.intersect? event_point.rectangle
     end
   end
 end
