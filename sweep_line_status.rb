149ebb48142566d7a1d5ff68c7d88e8ad4948768
Hendrik Bergunde <hbergunde@gmx.de>
2012-12-11 11:49:24 +0100

    rename sweepline_range

--- sweep_line_status.rb
+++ sweep_line_status.rb
@@ -1,8 +1,8 @@
 class SweepLineStatus < Array
   def report_intersection event_point
     self.each do |point_in_sls|
-      range = point_in_sls.rectangle.range
-      puts "Sweepline of #{point_in_sls.rectangle.name} intersects with sweepline of #{event_point.rectangle.name}" if range.include? event_point.x
+      sweepline_range = point_in_sls.rectangle.range
+      puts "Sweepline of #{point_in_sls.rectangle.name} intersects with sweepline of #{event_point.rectangle.name}" if sweepline_range.include? event_point.x
     end
   end
 end
